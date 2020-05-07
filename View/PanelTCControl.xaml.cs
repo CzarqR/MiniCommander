d6692a1ea3420b3444c8527c7d20d7699f5e902f
CzarqR <czarekr98@gmail.com>
2020-05-07 23:24:36 +0200

    init, base

---
+++ View/PanelTCControl.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Commander.View
+{
+
+    public partial class PanelTCControl : UserControl
+    {
+
+        #region properties
+
+
+
+        public string Path
+        {
+            get
+            {
+                return (string)GetValue(PathProperty);
+            }
+            set
+            {
+                SetValue(PathProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty PathProperty =
+            DependencyProperty.Register("Path", typeof(string), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+        public List<string> Drives
+        {
+            get
+            {
+                return (List<string>)GetValue(DrivesProperty);
+            }
+            set
+            {
+                SetValue(DrivesProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty DrivesProperty =
+            DependencyProperty.Register("Drives", typeof(List<string>), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+
+        public List<string> PathContent
+        {
+            get
+            {
+                return (List<string>)GetValue(PathContentProperty);
+            }
+            set
+            {
+                SetValue(PathContentProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty PathContentProperty =
+            DependencyProperty.Register("PathContent", typeof(List<string>), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+
+
+        public int SelectedDriveIndex
+        {
+            get
+            {
+                return (int)GetValue(SelectedDriveIndexProperty);
+            }
+            set
+            {
+                SetValue(SelectedDriveIndexProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty SelectedDriveIndexProperty =
+            DependencyProperty.Register("SelectedDriveIndex", typeof(int), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+
+        public ICommand SelectPath
+        {
+            get
+            {
+                return (ICommand)GetValue(SelectPathProperty);
+            }
+            set
+            {
+                SetValue(SelectPathProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty SelectPathProperty =
+            DependencyProperty.Register("SelectPath", typeof(ICommand), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+
+        //public int SelectedContentIndex
+        //{
+        //    get
+        //    {
+        //        return (int)GetValue(SelectedContentIndexProperty);
+        //    }
+        //    set
+        //    {
+        //        SetValue(SelectedContentIndexProperty, value);
+        //    }
+        //}
+
+        //public static readonly DependencyProperty SelectedContentIndexProperty =
+        //    DependencyProperty.Register("SelectedContentIndex", typeof(int), typeof(PanelTCControl), new PropertyMetadata(null));
+
+
+
+
+
+
+
+        #endregion
+
+        public PanelTCControl()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ ViewModel/ControlVM/PanelTCVM.cs
@@ -0,0 +1,135 @@
+﻿using Commander.ViewModel.Commands;
+using Commander.ViewModel.WindowVM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Commander.ViewModel.ControlVM
+{
+    class PanelTCVM : BaseVM
+    {
+        private string path;
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                path = value;
+                UpdateContent();
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+
+        private int selectedDriveIndex;
+
+        public int SelectedDriveIndex
+        {
+            get
+            {
+                return selectedDriveIndex;
+            }
+            set
+            {
+                selectedDriveIndex = value;
+                Path = Drivers[SelectedDriveIndex];
+                Console.WriteLine(Path);
+                OnPropertyChanged(nameof(SelectedDriveIndex));
+            }
+        }
+
+        private List<string> drivers;
+
+        public List<string> Drivers
+        {
+            get
+            {
+                return drivers;
+            }
+            set
+            {
+                drivers = value;
+                OnPropertyChanged(nameof(Drivers));
+            }
+        }
+
+        private List<string> content;
+        public List<string> Content
+        {
+            get
+            {
+                return content;
+            }
+            set
+            {
+                content = value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+
+        private ICommand pathSelectCommand;
+
+        public ICommand PathSelectCommand
+        {
+            get
+            {
+                return pathSelectCommand ?? (pathSelectCommand = new CommandHandler(Click, () => { return true; }));
+            }
+
+        }
+
+        public void Click()
+        {
+            Console.WriteLine("SELECTED");
+        }
+
+
+        private List<string> contentOriginal;
+        private int foldersCount;
+
+        public PanelTCVM()
+        {
+            Drivers = Directory.GetLogicalDrives().OfType<string>().ToList();
+            SelectedDriveIndex = 1; //todo, change to 0 after testing
+            Path = Drivers[SelectedDriveIndex];
+        }
+
+        private void UpdateContent()
+        {
+            Console.WriteLine("Update");
+            List<string> folders = Directory.GetDirectories(Path).OfType<string>().ToList();
+            foldersCount = folders.Count;
+            List<string> files = Directory.GetFiles(Path).OfType<string>().ToList();
+            contentOriginal = folders.Concat(files).ToList();
+
+
+            for (int i = 0; i < folders.Count; i++)
+            {
+                folders[i] = "<" + folders[i][0] + ">" + folders[i].Substring(3);
+            }
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                files[i] = files[i].Substring(3);
+            }
+
+            Content = folders.Concat(files).ToList();
+
+            //if (leftInsideIterator != 0)
+            //{
+            //    LeftContent.Insert(0, "...");
+            //}
+
+        }
+
+
+
+    }
+}
